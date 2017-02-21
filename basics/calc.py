87e6b2eef6c5fbb2f2c3f1a7fd81c4b85f968417
Karol Chmielewski <chmielewski.karol.96@gmail.com>
2017-02-21 21:37:47 +0100

    lessons

--- basics/calc.py
+++ basics/calc.py
@@ -2,7 +2,7 @@
 class Calculator():
 
     def __init__(self):
-        print("init")
+        self.ostatni_wynik = 0
 
     def __del__(self):
         print("del")
@@ -21,10 +21,25 @@ class Calculator():
 
     def dodaj(self, a, b):
         wynik = a + b
+        self.ostatni_wynik = wynik
         print(wynik)
 
     def odejmij(self, a, b):
         wynik = a - b
+        selg.ostatni_wynik = wynik
         print(wynik)
 
 calc = Calculator()
+
+calc.dodaj(5,3)
+calc.dodaj(7,3)
+calc.dodaj(8,3)
+
+calc2 = Calculator()
+
+calc2.dodaj(5,5)
+calc2.dodaj(7,2)
+calc2.dodaj(8,8)
+
+print("Ostatni wynik Twoich obliczeń: {}".format(calc.ostatni_wynik))
+print("Ostatni wynik Twoich obliczeń: {}".format(calc2.ostatni_wynik))
