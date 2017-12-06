a969d6c4e393cb099232324f291ce59cc485fc5a
kchmielewski <chmielewski.karol.96@gmail.com>
2017-12-06 22:31:38 +0100

    Delete 2.py, 3.py

--- basics/typy danych/2.py
+++
@@ -1,7 +0,0 @@
-'''Stwórz zmienną x przypisując do niej dowolną wartość, a następnie korzystając z funkcji isinstance
- sprawdź czy przypisana wartość jest instancją typu int.'''
-
-x = 1
-
-if isinstance(x,int):
-    print("To jest int")
