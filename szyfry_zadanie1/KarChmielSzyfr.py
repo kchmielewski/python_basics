ff88579a228ca9c56ce526f1f41c42404e295d79
Karol <chmielewski.karol.96@gmail.com>
2018-10-26 17:30:55 +0200

    .

--- szyfry_zadanie1/KarChmielSzyfr.py
+++ szyfry_zadanie1/KarChmielSzyfr.py
@@ -10,11 +10,11 @@ class Cipher:
         self.alphabet2Upper = dict(zip(range(26), "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
         self.alphabet2Lower = dict(zip(range(26), "abcdefghijklmnopqrstuvwxyz"))
 
-        self.swap = dict(zip([ord("ą"), ord("ć"), ord("ę"), ord("ó"), ord("ł"), ord("ż"), ord("ź"), ord("ń")],
-                           ["a", "c", "e", "o", "l", "z", "z", "n"]))
-        self.swap2 = dict(zip([ord("Ą"), ord("Ć"), ord("Ę"), ord("Ó"), ord("Ł"), ord("Ż"), ord("Ź"), ord("Ń")],
-                            ["A", "C", "E", "O", "L", "Z", "Z", "N"]))
-    
+        self.swap = dict(zip(
+            [ord("ą"), ord("ć"), ord("ę"), ord("ł"), ord("ń"), ord("ó"), ord("ś"), ord("ź"), ord("ż"),
+             ord("Ą"), ord("Ć"), ord("Ę"), ord("Ł"), ord("Ń"), ord("Ó"), ord("Ś"), ord("Ż"), ord("Ź")],
+            ["a", "c", "e", "l", "n", "o", "s", "z", "z", "A", "C", "E", "L", "N", "O", "S", "Z", "Z"]))
+
     @staticmethod
     def nwd(a, mod):
         while mod:
@@ -41,17 +41,17 @@ class Ceasar(Cipher):
     def encoding(self, public, key):
         encrypted = ""
         public = self.removeEmptyLines(public)
-
         for letter in public:
+            letter = letter.translate(self.swap)
             if letter.isalpha():
                 if letter.isupper():
-                    encrypted += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap2)] + key) % self.mod]
+                    encrypted += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap)] + key) % self.mod]
                 else:
                     encrypted += self.alphabet2Lower[(self.alphabetLower[letter.translate(self.swap)] + key) % self.mod]
             else:
                 encrypted += letter
 
-        with open("crypto.txt", "w") as crypto, open("extra.txt", "w") as extra:
+        with open("crypto.txt", "w", encoding="utf-8") as crypto, open("extra.txt", "w", encoding="utf-8") as extra:
             crypto.write(encrypted)
             for letter in public.strip()[:3]:
                 if letter.isalpha():
@@ -64,13 +64,13 @@ class Ceasar(Cipher):
         for letter in text:
             if letter.isalpha():
                 if letter.isupper():
-                    decrypted += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap2)] - key) % self.mod]
+                    decrypted += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap)] - key) % self.mod]
                 else:
                     decrypted += self.alphabet2Lower[(self.alphabetLower[letter.translate(self.swap)] - key) % self.mod]
             else:
                 decrypted += letter
 
-        with open("decrypt.txt", "w") as decrypt:
+        with open("decrypt.txt", "w", encoding="utf-8") as decrypt:
             decrypt.write(decrypted)
 
     def cryptoanalysisPublic(self, helper, encrypted):
@@ -86,11 +86,11 @@ class Ceasar(Cipher):
             i += 1
 
             if helper[0].isupper():
-                text = self.alphabet2Upper[(self.alphabetUpper[helper[0].translate(self.swap2)] + i) % self.mod]
+                text = self.alphabet2Upper[(self.alphabetUpper[helper[0].translate(self.swap)] + i) % self.mod]
             else:
                 text = self.alphabet2Lower[(self.alphabetLower[helper[0].translate(self.swap)] + i) % self.mod]
 
-        with open("key-new.txt", "w") as keynew:
+        with open("key-new.txt", "w", encoding="utf-8") as keynew:
             keynew.write(str(i))
             self.decrypting(encrypted,i)
 
@@ -98,12 +98,12 @@ class Ceasar(Cipher):
         text = ""
         encrypted = self.removeEmptyLines(encrypted)
 
-        with open("decrypt.txt", "w") as decrypt:
+        with open("decrypt.txt", "w", encoding="utf-8") as decrypt:
             for i in range(26):
                 for letter in encrypted:
                     if letter.isalpha():
                         if letter.isupper():
-                            text += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap2)] - i) % self.mod]
+                            text += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap)] - i) % self.mod]
                         else:
                             text += self.alphabet2Lower[(self.alphabetLower[letter.translate(self.swap)] - i) % self.mod]
                     else:
@@ -125,17 +125,17 @@ class Affine(Cipher):
             for letter in public:
                 if letter.isalpha():
                     if letter.isupper():
-                        encrypted += self.alphabet2Upper[((self.alphabetUpper[letter.translate(self.swap2)] * a) + b) % self.mod]
+                        encrypted += self.alphabet2Upper[((self.alphabetUpper[letter.translate(self.swap)] * a) + b) % self.mod]
                     else:
                         encrypted += self.alphabet2Lower[((self.alphabetLower[letter.translate(self.swap)] * a) + b) % self.mod]
                 else:
                     encrypted += letter
 
-        with open("crypto.txt", "w") as crypto, open("extra.txt", "w") as extra:
+        with open("crypto.txt", "w", encoding="utf-8") as crypto, open("extra.txt", "w", encoding="utf-8") as extra:
             crypto.write(encrypted)
             for letter in public.strip()[:3]:
                 if letter.isalpha():
-                    extra.write(public[:2])
+                    extra.write(letter)
 
     def decrypting(self, text, a, b):
         a2 = self.findReverseA(a)
@@ -148,13 +148,13 @@ class Affine(Cipher):
         for letter in text:
             if letter.isalpha():
                 if letter.isupper():
-                    decrypted += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap2)] * a2 + b2) % self.mod]
+                    decrypted += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap)] * a2 + b2) % self.mod]
                 else:
                     decrypted += self.alphabet2Lower[(self.alphabetLower[letter.translate(self.swap)] * a2 + b2) % self.mod]
             else:
                 decrypted += letter
 
-        with open("decrypt.txt", "w") as decrypt:
+        with open("decrypt.txt", "w", encoding="utf-8") as decrypt:
             decrypt.write(decrypted)
 
     def cryptoanalysisPublic(self, helper, encrypted):
@@ -167,18 +167,18 @@ class Affine(Cipher):
             if a % 2 != 0 and a != 13:
                 for b in range(1, self.mod + 1):
                     if helper[0].isupper():
-                        decr1 = self.alphabet2Upper[(self.alphabetUpper[helper[0].translate(self.swap2)] * a + b) % self.mod]
+                        decr1 = self.alphabet2Upper[(self.alphabetUpper[helper[0].translate(self.swap)] * a + b) % self.mod]
                     else:
                         decr1 = self.alphabet2Lower[(self.alphabetLower[helper[0].translate(self.swap)] * a + b) % self.mod]
 
                     if helper[1].isupper():
-                        decr2 = self.alphabet2Upper[(self.alphabetUpper[helper[1].translate(self.swap2)] * a + b) % self.mod]
+                        decr2 = self.alphabet2Upper[(self.alphabetUpper[helper[1].translate(self.swap)] * a + b) % self.mod]
                     else:
                         decr2 = self.alphabet2Lower[(self.alphabetLower[helper[1].translate(self.swap)] * a + b) % self.mod]
 
                     if decr1 == encr1 and decr2 == encr2:
-                        with open("key-new.txt", "w") as keynew:
-                            keynew.write(f"{a} {b}")
+                        with open("key-new.txt", "w", encoding="utf-8") as keynew:
+                            keynew.write("{} {}".format(a, b))
                             self.decrypting(encrypted, a, b)
                             return
 
@@ -186,14 +186,14 @@ class Affine(Cipher):
         decrypted = ""
         text = self.removeEmptyLines(text)
 
-        with open("decrypt.txt", "w") as decrypt:
+        with open("decrypt.txt", "w", encoding="utf-8") as decrypt:
             for a in range(1, self.mod + 1):
                 if a % 2 != 0 and a != 13:
                     for b in range(1, self.mod + 1):
                         for letter in text:
                             if letter.isalpha():
                                 if letter.isupper():
-                                    decrypted += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap2)] * a + b) % self.mod]
+                                    decrypted += self.alphabet2Upper[(self.alphabetUpper[letter.translate(self.swap)] * a + b) % self.mod]
                                 else:
                                     decrypted += self.alphabet2Lower[(self.alphabetLower[letter.translate(self.swap)] * a + b) % self.mod]
                             else:
@@ -206,7 +206,7 @@ class Affine(Cipher):
 if "-e" in sys.argv and "-c" in sys.argv:
 
     ceasar = Ceasar()
-    with open("plain.txt", "r") as plain, open("key.txt", "r") as key:
+    with open("plain.txt", "r", encoding="utf-8") as plain, open("key.txt", "r", encoding="utf-8") as key:
         keys = key.read().split(" ")
         a = int(keys[0])
         if a > 26 or a < 1:
@@ -217,7 +217,7 @@ if "-e" in sys.argv and "-c" in sys.argv:
 elif "-e" in sys.argv and "-a" in sys.argv:
 
     aff = Affine()
-    with open("key.txt", "r") as key, open("plain.txt", "r") as plain:
+    with open("key.txt", "r", encoding="utf-8") as key, open("plain.txt", "r", encoding="utf-8") as plain:
         keys = key.read().split(" ")
         a, b = int(keys[0]), int(keys[1])
         if a > 26 or b > 26 or a < 1 or b < 1:
@@ -228,7 +228,7 @@ elif "-e" in sys.argv and "-a" in sys.argv:
 elif "-d" in sys.argv and "-c" in sys.argv:
 
     ceasar = Ceasar()
-    with open("crypto.txt", "r") as crypto, open("key.txt", "r") as key:
+    with open("crypto.txt", "r", encoding="utf-8") as crypto, open("key.txt", "r", encoding="utf-8") as key:
         keys = key.read().split(" ")
         a = int(keys[0])
         if a > 26 or a < 1:
@@ -238,7 +238,7 @@ elif "-d" in sys.argv and "-c" in sys.argv:
 elif "-d" in sys.argv and "-a" in sys.argv:
 
     aff = Affine()
-    with open("key.txt", "r") as key, open("crypto.txt", "r") as crypto:
+    with open("key.txt", "r", encoding="utf-8") as key, open("crypto.txt", "r", encoding="utf-8") as crypto:
         keys = key.read().split(" ")
         a, b = int(keys[0]), int(keys[1])
         if a > 26 or b > 26 or a < 1 or b < 1:
@@ -249,25 +249,25 @@ elif "-d" in sys.argv and "-a" in sys.argv:
 elif "-j" in sys.argv and "-c" in sys.argv:
 
     ceasar = Ceasar()
-    with open("extra.txt", "r") as extra, open("crypto.txt", "r") as crypto:
+    with open("extra.txt", "r", encoding="utf-8") as extra, open("crypto.txt", "r", encoding="utf-8") as crypto:
         ceasar.cryptoanalysisPublic(extra.read(), crypto.read())
 
 elif "-j" in sys.argv and "-a" in sys.argv:
 
     aff = Affine()
-    with open("extra.txt", "r") as extra, open("crypto.txt", "r") as crypto:
+    with open("extra.txt", "r", encoding="utf-8") as extra, open("crypto.txt", "r", encoding="utf-8") as crypto:
         aff.cryptoanalysisPublic(extra.read(), crypto.read())
 
 elif "-k" in sys.argv and "-c" in sys.argv:
 
     ceasar = Ceasar()
-    with open("crypto.txt", "r") as crypto:
+    with open("crypto.txt", "r", encoding="utf-8") as crypto:
         ceasar.cryptoanalysisCryptogram(crypto.read())
 
 elif "-k" in sys.argv and "-a" in sys.argv:
 
     aff = Affine()
-    with open("crypto.txt", "r") as crypto:
+    with open("crypto.txt", "r", encoding="utf-8") as crypto:
         aff.cryptoanalysisCryptogram(crypto.read())
 
 else:
